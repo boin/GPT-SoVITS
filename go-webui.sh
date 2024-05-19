50d6587e92081cb44637644d3c834d40ed47b800
boin <mcfeeboin@gmail.com>
2024-05-19 14:42:53 +0000

    changed logread path

--- go-webui.sh
+++ go-webui.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-touch TEMP/syslog
+touch -f TEMP/syslog
 # Redirect stdout ( > ) into a named pipe ( >() ) running "tee"
 
 exec > >(tee -i TEMP/syslog)
--- tools/webconsole.py
+++ tools/webconsole.py
@@ -1,16 +1,7 @@
 import subprocess
 import socket, os
 
-def find_file_with_prefix(directory, prefix):
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            if file.startswith(prefix):
-                return os.path.join(root, file)
-
 def get_docker_logs():
-    base_path = "/workspace/containers"
-    container_id = socket.gethostname()
-    log_path = find_file_with_prefix(base_path, container_id) 
-    #if not log_path:
-    #    log_path = '/home/coder/pay.sql'
+    base_path = "./TEMP/syslog"
+    log_path = (base_path) 
     return log_path
