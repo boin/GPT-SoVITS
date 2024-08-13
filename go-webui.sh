ce3ee720c01df9b1edc95791c980ce0916596670
boin <mcfeeboin@gmail.com>
2024-08-13 09:32:05 +0000

    fixed log output

--- go-webui.sh
+++ go-webui.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-mkdir -p logs
-touch -f logs/app.log
-
-python webui.py 2>&1 | tee -i logs/app.log
+mkdir -p logs && python -u webui.py 2>&1 | stdbuf -oL -eL tee -i logs/app.log
